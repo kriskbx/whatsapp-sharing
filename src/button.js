217dce1c125d8f26f59728c85a9c8527abb0ab1f
moklick <info@moritzklack.com>
2015-01-12 17:02:59 +0100

    Remove unnecessary ternary operation.

--- src/button.js
+++ src/button.js
@@ -20,7 +20,7 @@ waShBtn = function() {
 /**
  * Check for iOS
  */
-waShBtn.prototype.isIos = ((navigator.userAgent.match(/Android|iPhone/i) && !navigator.userAgent.match(/iPod|iPad/i)) ? true : false);
+waShBtn.prototype.isIos = navigator.userAgent.match(/Android|iPhone/i) && !navigator.userAgent.match(/iPod|iPad/i);
 
 /**
  * Call a function when the content is loaded and the document is ready
